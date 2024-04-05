db89ed18334a72ff9ab66d577639c437bd24ed7d
akirkey2 <112200926+akirkey2@users.noreply.github.com>
2024-04-04 17:46:32 -0700

    Update AESO_grid_data_analysis.py
    cleaned up the code a little and used the plotly area plots. They plot well when use the line groups but are buggy without. Consider grouping the dataset so you can plot without the line groups (much clearner)

--- AESO_grid_data_analysis.py
+++ AESO_grid_data_analysis.py
@@ -18,6 +18,9 @@ from matplotlib import cm
 import pathlib
 import os
 from sklearn import preprocessing
+import plotly.io as pio
+import plotly.express as px
+pio.renderers.default='browser'
 mpl.rcParams.update(mpl.rcParamsDefault)
 plt.rc('axes', axisbelow=True)
 mpl.interactive(True)
@@ -29,20 +32,27 @@ windows_file_path = 'C:\\Users\\Aaron Kirkey\\Documents\\GitHub\\AESO-data\\CSD 
 ubuntu_file_path = ''
 df = pd.read_csv(windows_file_path)
 #%%
-print(df['Fuel Type'].unique())
-#%%
-dftw  = df.loc[(df['Date (MST)'].str.contains('2024-01'))]
+dftw  = df.loc[(df['Date (MST)'].str.contains('2024-01-01'))]
 dftotal = dftw.groupby(['Date (MST)']).sum()
 dftotal.index = pd.to_datetime(dftotal.index)
 #%%
+lst = []
+df_temp_2 = pd.DataFrame()
 for i in df['Fuel Type'].unique():
     print(i)
+    lst.append(i)
     df_temp = dftw.loc[(df['Fuel Type'].str.contains(i))] 
     df_temp = df_temp.groupby(['Date (MST)'])[['Date (MST)','Volume']].sum()
     df_temp['Time'] = df_temp.index.str.slice(11,16)
+    df_temp_2[i] = df_temp["Volume"]
     df_temp.index = pd.to_datetime(df_temp.index)
     plt.plot(df_temp.index,df_temp['Volume'],label = i)
 plt.plot(dftotal.index,dftotal['Volume'],label = 'Total', color = 'black')
+plt.xticks(rotation = 45)
 plt.title('Hourly Generation on AESO Jan 2024')
 plt.legend(loc = 1)
 plt.show()
+
+#%%
+fig = px.area(dftw, x="Date (MST)", y="Volume", color="Fuel Type", line_group='Asset Name')
+fig.show()
